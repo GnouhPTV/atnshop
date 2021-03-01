a3f89df98d55c25ffd2ad7a3696073e9bf14d2dc
GnouhPTV <78476883+GnouhPTV@users.noreply.github.com>
2021-03-01 23:06:23 +0700

    Update login2.php

--- login2.php
+++ login2.php
@@ -42,7 +42,7 @@ if (!$pg_heroku)
 {
   die('Error: Could not connect: ' . pg_last_error());
 }
- $query = 'select * from atnshop1';
+ $query = 'select * from atnshop2';
 $data = pg_query($pg_heroku, $query);
  $total = pg_num_rows($data);
 if($total!=0)
