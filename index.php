b67f75e0f2d611301ad1d927f98397f1be1de41b
GnouhPTV <78476883+GnouhPTV@users.noreply.github.com>
2021-03-01 10:20:32 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,18 +1,17 @@
 <html>
   <head>
-    <link rel="stylesheet" href ="styles.css">
     <title> ATN Shop </title>
- <ul>
- <li> <a href="login.php">Login </a></li>
- </ul>
-    </head>
- <body>
- <style>
- body {
- background-image: url('background.jpg');
- background-attachment: fixed;
- background-size: 100%100%;
- }
- </style>
- </body>
+    <ul>
+      <li> <a href="login.php">Login </a></li>
+    </ul>
+  </head>
+  <body>
+    <style>
+      body {
+        background-image: url('background.jpg');
+        background-attachment: fixed;
+        background-size: 100%100%;
+      }
+    </style>
+  </body>
 </html>
